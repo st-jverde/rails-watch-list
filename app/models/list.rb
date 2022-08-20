0d254bc172a2638b5e8981b33e79cad0a0646a2f
Joeri Verdegaal <joeriverdegaal@gmail.com>
2022-08-21 00:50:55 +0200

    added delete list butten | installed cloudinairy and added to schema. Doesn't work yet

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,8 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 
+  has_one_attached :photo
+
   validates :name, uniqueness: true
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: %i[index new create show] do
+  resources :lists, only: %i[index new create show destroy] do
     member  do
       resources :bookmarks, only: %i[new create]
     end
   end
   resources :bookmarks, only: :destroy
 end
+
