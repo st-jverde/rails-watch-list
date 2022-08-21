adcddc746c5bff30d24d82effcdd9125d4db0977
Joeri Verdegaal <joeriverdegaal@gmail.com>
2022-08-21 11:17:49 +0200

    worked on adding images. Not sure if it worked

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,7 @@
 class List < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
-
-  has_one_attached :photo
 
   validates :name, uniqueness: true
   validates :name, presence: true
